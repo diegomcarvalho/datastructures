278c941c5aece4a60ed8ceb806413c8bedead540
Diego Carvalho <carvalho@Diegos-MacBook-Pro.local>
2019-01-15 19:07:25 -0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datastructures',
+      version='0.1',
+      description='Abstract Data Types',
+      url='https://github.com/diegomcarvalho/datastructures.git',
+      author='Diego Carvalho',
+      author_email='d.carvalho@ieee.org',
+      license='MIT',
+      packages=['datastructures'],
+      zip_safe=False)
